33ef4ffae19c769fe94c8319400f0486d9dd1d62
KARTHICKKUMAR S <veerakarthick235@gmail.com>
2026-05-30 11:35:20 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // @ts-ignore: Next.js 16 types are sometimes out of sync with actual Turbopack config options
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
